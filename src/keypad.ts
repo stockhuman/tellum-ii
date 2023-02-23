87aad704a3f1fcc0dfe588fc0cae1fe6f53df56c
Michael Hemingway <mikemingway@gmail.com>
2023-02-23 15:41:55 -0500

    Map keypad

--- src/keypad.ts
+++ src/keypad.ts
@@ -3,17 +3,18 @@ import { Gpio } from 'onoff'
 
 import { ePin } from './util/gpio'
 
+export const available = Gpio.accessible
 export const kbe = new EventEmitter()
 
-const selectA = new Gpio(ePin(1), 'out')
+const selectA = new Gpio(ePin(3), 'out')
 const selectB = new Gpio(ePin(2), 'out')
-const selectC = new Gpio(ePin(3), 'out')
+const selectC = new Gpio(ePin(1), 'out')
 const columns = [selectA, selectB, selectC]
 
 const readA = new Gpio(ePin(0), 'in')
-const readB = new Gpio(ePin(4), 'in')
+const readB = new Gpio(ePin(6), 'in')
 const readC = new Gpio(ePin(5), 'in')
-const readD = new Gpio(ePin(6), 'in')
+const readD = new Gpio(ePin(4), 'in')
 const rows = [readA, readB, readC, readD]
 
 const keypadPins: Gpio[] = [...rows, ...columns]
@@ -34,7 +35,9 @@ let scanInterval = null
 let selectPin = 0
 
 function registerKeypad() {
-  console.info(`GPIO is${Gpio.accessible ? '' : ' not'} available`)
+  // reset states (if applicable)
+  columns.forEach(col => col.writeSync(Gpio.LOW))
+
   selectA.writeSync(1) // scanning initial state
   setTimeout(() => {
     // sync scanning to logic
@@ -52,29 +55,28 @@ function scan() {
 }
 
 function determineKeyPressed() {
-  let pinA = Boolean(readA.readSync())
-  let pinB = Boolean(readB.readSync())
-  let pinC = Boolean(readC.readSync())
-  let pinD = Boolean(readD.readSync())
+  let pinA = Boolean(readA.readSync()) // pin 0
+  let pinB = Boolean(readB.readSync()) // pin 6
+  let pinC = Boolean(readC.readSync()) // pin 5
+  let pinD = Boolean(readD.readSync()) // pin 4
   if (pinA || pinB || pinC || pinD) console.log(selectPin, '=>', pinA, pinB, pinC, pinD)
   switch (selectPin) {
-    /** @TODO determine actual pin-to-letter combos */
-    case 0:
-      checkState(pinA, '0')
-      checkState(pinB, '1')
-      checkState(pinC, '2')
-      checkState(pinD, '3')
+    case 0: // selectA, pin #3
+      checkState(pinA, '1')
+      checkState(pinB, '4')
+      checkState(pinC, '7')
+      checkState(pinD, '*')
       break
-    case 1:
-      checkState(pinA, '4')
+    case 1: // selectB, pin #2
+      checkState(pinA, '2')
       checkState(pinB, '5')
-      checkState(pinC, '6')
-      checkState(pinD, '7')
+      checkState(pinC, '8')
+      checkState(pinD, '0')
       break
-    case 2:
-      checkState(pinA, '8')
-      checkState(pinB, '9')
-      checkState(pinC, '*')
+    case 2: // selectC, pin #1
+      checkState(pinA, '3')
+      checkState(pinB, '6')
+      checkState(pinC, '9')
       checkState(pinD, '#')
       break
 
