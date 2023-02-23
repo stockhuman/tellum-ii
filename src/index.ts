c798bd89b460340737ee96788ca3014ae996545c
Michael Hemingway <mikemingway@gmail.com>
2023-02-22 19:02:44 -0500

    log events (dev)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { registerKeypad } from './keypad';
+import { registerKeypad, kbe } from './keypad';
 
 async function setup () {
   registerKeypad()
 }
+
+kbe.on('keyPressed', console.log)
 
 setup()
