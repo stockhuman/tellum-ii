130b6fc42f0db3472152e6ab9d56960a2637273b
Michael Hemingway <mikemingway@gmail.com>
2023-02-24 15:33:14 -0500

    Update keypad.ts

--- src/keypad.ts
+++ src/keypad.ts
@@ -4,20 +4,6 @@ import { SerialPort } from 'serialport'
 export const kbe = new EventEmitter()
 const serial = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600, autoOpen: true })
 
-const keyStates = {
-  '0': false,
-  '1': false,
-  '2': false,
-  '3': false,
-  '4': false,
-  '5': false,
-  '6': false,
-  '7': false,
-  '9': false,
-  '*': false,
-  '#': false,
-}
-
 function registerKeypad() {
   // SerialPort.list().then(console.log)
   serial.on('data', readBuf)
