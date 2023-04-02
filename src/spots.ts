e3030c43d11310bbb7a779c16ceb1b8128ecefeb
Michael Hemingway <mpghemingway@gmail.com>
2023-04-02 19:27:37 -0400

    Update spots.ts

--- src/spots.ts
+++ src/spots.ts
@@ -1,5 +1,18 @@
 const spots = [
-  { x: 0, y: 0, file: 'intro' }
+  { x: 2, y: 12, file: 'point_azealiabanks' },
+  { x: 19, y: 96, file: 'point_im27yearsoldnoprospects' },
+  { x: 9, y: 9, file: 'point_luftballoons' },
+  { x: 41, y: 2, file: 'point_discoelysium' },
+  { x: 16, y: 99, file: 'point_amishparadise' },
+  { x: 1, y: 61, file: 'point_goldenratio' },
+  { x: 14, y: 88, file: 'point_rihannawhatareyoudoinghere' },
+  { x: 19, y: 73, file: 'point_jamesblunt' },
+  { x: 19, y: 97, file: 'point_baby' },
+  { x: 40, y: 3, file: 'point_savingme' },
+  { x: 196, y: 96, file: 'point_gold' },
+  { x: 20, y: 3, file: 'point_whatsfordinner' },
+  { x: 3, y: 14, file: 'point_pi' },
+  { x: 3, y: 43, file: 'point_halo' },
 ]
 
 export default spots
